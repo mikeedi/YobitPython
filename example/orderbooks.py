c338e70689c62afd32f2176d3c1bf73ff01c4682
siredukov <siredukov@gmail.com>
2018-01-28 19:48:37 +0300

    fixed import

--- example/orderbooks.py
+++ example/orderbooks.py
@@ -1,3 +1,8 @@
+# add path to find Yobit module
+import sys 
+sys.path.append("../")
+
+
 from Yobit.api import YoBit
 import pandas as pd
 import argparse
@@ -37,5 +42,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--pairs', type=str)
     parser.add_argument('--limit', type=int, default=10)
-    print(parser.parse_args())
     main(config=parser.parse_args())
